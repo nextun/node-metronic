5975d8f724781e1628eae6491c8bf4176b95de00
Mark Bell <mark.bell@noanmeit.co.uk>
2017-06-26 19:17:52 +0100

    Updated daterangepicker

---
+++ gulpfile.js
@@ -0,0 +1,193 @@
+'use strict';
+
+// sass compile
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var prettify = require('gulp-prettify');
+var minifyCss = require("gulp-minify-css");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var rtlcss = require("gulp-rtlcss");  
+var connect = require('gulp-connect');
+var concat = require('gulp-concat');
+
+var postcss = require('gulp-postcss'),
+scripts = require('./gulp/scripts.js'),
+watch = require('gulp-watch'),
+styles = require('./gulp/styles'),
+php = require('gulp-connect-php'),
+browserSync = require('browser-sync'),
+reload  = browserSync.reload;
+
+
+//*** Localhost server tast
+// gulp.task('localhost', function() {
+//   connect.server();
+// });
+//
+// gulp.task('localhost-live', function() {
+//   connect.server({
+//     livereload: true
+//   });
+// });
+
+
+
+// gulp.start('scripts');
+//*** SASS compiler task
+gulp.task('sass', function () {
+  // bootstrap compilation
+	gulp.src('./sass/bootstrap.scss').pipe(sass()).pipe(gulp.dest('./assets/global/plugins/bootstrap/css/'));
+
+  // select2 compilation using bootstrap variables
+	gulp.src('./assets/global/plugins/select2/sass/select2-bootstrap.min.scss').pipe(sass({outputStyle: 'compressed'})).pipe(gulp.dest('./assets/global/plugins/select2/css/'));
+
+  // global theme stylesheet compilation
+	gulp.src('./sass/global/*.scss').pipe(sass()).pipe(gulp.dest('./assets/global/css'));
+	gulp.src('./sass/apps/*.scss').pipe(sass()).pipe(gulp.dest('./assets/apps/css'));
+	gulp.src('./sass/pages/*.scss').pipe(sass()).pipe(gulp.dest('./assets/pages/css'));
+
+  // theme layouts compilation
+	gulp.src('./sass/layouts/layout/*.scss').pipe(sass()).pipe(gulp.dest('./assets/layouts/layout/css'));
+  gulp.src('./sass/layouts/layout/themes/*.scss').pipe(sass()).pipe(gulp.dest('./assets/layouts/layout/css/themes'));
+
+  gulp.src('./sass/layouts/layout2/*.scss').pipe(sass()).pipe(gulp.dest('./assets/layouts/layout2/css'));
+  gulp.src('./sass/layouts/layout2/themes/*.scss').pipe(sass()).pipe(gulp.dest('./assets/layouts/layout2/css/themes'));
+
+  gulp.src('./sass/layouts/layout3/*.scss').pipe(sass()).pipe(gulp.dest('./assets/layouts/layout3/css'));
+  gulp.src('./sass/layouts/layout3/themes/*.scss').pipe(sass()).pipe(gulp.dest('./assets/layouts/layout3/css/themes'));
+
+  gulp.src('./sass/layouts/layout4/*.scss').pipe(sass()).pipe(gulp.dest('./assets/layouts/layout4/css'));
+  gulp.src('./sass/layouts/layout4/themes/*.scss').pipe(sass()).pipe(gulp.dest('./assets/layouts/layout4/css/themes'));
+
+  gulp.src('./sass/layouts/layout5/*.scss').pipe(sass()).pipe(gulp.dest('./assets/layouts/layout5/css'));
+
+  gulp.src('./sass/layouts/layout6/*.scss').pipe(sass()).pipe(gulp.dest('./assets/layouts/layout6/css'));
+
+  gulp.src('./sass/layouts/layout7/*.scss').pipe(sass()).pipe(gulp.dest('./assets/layouts/layout7/css'));
+});
+
+//*** SASS watch(realtime) compiler task
+gulp.task('sass:watch', function () {
+	gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+//*** CSS & JS minify task
+gulp.task('minify', function () {
+    // css minify 
+    gulp.src(['./assets/apps/css/*.css', '!./assets/apps/css/*.min.css']).pipe(minifyCss()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('./assets/apps/css/'));
+
+    gulp.src(['./assets/global/css/*.css','!./assets/global/css/*.min.css']).pipe(minifyCss()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('./assets/global/css/'));
+    gulp.src(['./assets/pages/css/*.css','!./assets/pages/css/*.min.css']).pipe(minifyCss()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('./assets/pages/css/'));    
+    
+    gulp.src(['./assets/layouts/**/css/*.css','!./assets/layouts/**/css/*.min.css']).pipe(rename({suffix: '.min'})).pipe(minifyCss()).pipe(gulp.dest('./assets/layouts/'));
+    gulp.src(['./assets/layouts/**/css/**/*.css','!./assets/layouts/**/css/**/*.min.css']).pipe(rename({suffix: '.min'})).pipe(minifyCss()).pipe(gulp.dest('./assets/layouts/'));
+
+    gulp.src(['./assets/global/plugins/bootstrap/css/*.css','!./assets/global/plugins/bootstrap/css/*.min.css']).pipe(minifyCss()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('./assets/global/plugins/bootstrap/css/'));
+
+    //js minify
+    gulp.src(['./assets/apps/scripts/*.js','!./assets/apps/scripts/*.min.js']).pipe(uglify()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('./assets/apps/scripts/'));
+    gulp.src(['./assets/global/scripts/*.js','!./assets/global/scripts/*.min.js']).pipe(uglify()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('./assets/global/scripts'));
+    gulp.src(['./assets/pages/scripts/*.js','!./assets/pages/scripts/*.min.js']).pipe(uglify()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('./assets/pages/scripts'));
+    gulp.src(['./assets/layouts/**/scripts/*.js','!./assets/layouts/**/scripts/*.min.js']).pipe(uglify()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('./assets/layouts/'));
+});
+
+//*** RTL converter task
+gulp.task('rtlcss', function () {
+
+  gulp
+    .src(['./assets/apps/css/*.css', '!./assets/apps/css/*-rtl.min.css', '!./assets/apps/css/*-rtl.css', '!./assets/apps/css/*.min.css'])
+    .pipe(rtlcss())
+    .pipe(rename({suffix: '-rtl'}))
+    .pipe(gulp.dest('./assets/apps/css'));
+
+  gulp
+    .src(['./assets/pages/css/*.css', '!./assets/pages/css/*-rtl.min.css', '!./assets/pages/css/*-rtl.css', '!./assets/pages/css/*.min.css'])
+    .pipe(rtlcss())
+    .pipe(rename({suffix: '-rtl'}))
+    .pipe(gulp.dest('./assets/pages/css'));
+
+  gulp
+    .src(['./assets/global/css/*.css', '!./assets/global/css/*-rtl.min.css', '!./assets/global/css/*-rtl.css', '!./assets/global/css/*.min.css'])
+    .pipe(rtlcss())
+    .pipe(rename({suffix: '-rtl'}))
+    .pipe(gulp.dest('./assets/global/css'));
+
+  gulp
+    .src(['./assets/layouts/**/css/*.css', '!./assets/layouts/**/css/*-rtl.css', '!./assets/layouts/**/css/*-rtl.min.css', '!./assets/layouts/**/css/*.min.css'])
+    .pipe(rtlcss())
+    .pipe(rename({suffix: '-rtl'}))
+    .pipe(gulp.dest('./assets/layouts'));
+
+  gulp
+    .src(['./assets/layouts/**/css/**/*.css', '!./assets/layouts/**/css/**/*-rtl.css', '!./assets/layouts/**/css/**/*-rtl.min.css', '!./assets/layouts/**/css/**/*.min.css'])
+    .pipe(rtlcss())
+    .pipe(rename({suffix: '-rtl'}))
+    .pipe(gulp.dest('./assets/layouts'));
+
+  gulp
+    .src(['./assets/global/plugins/bootstrap/css/*.css', '!./assets/global/plugins/bootstrap/css/*-rtl.css', '!./assets/global/plugins/bootstrap/css/*.min.css'])
+    .pipe(rtlcss())
+    .pipe(rename({suffix: '-rtl'}))
+    .pipe(gulp.dest('./assets/global/plugins/bootstrap/css')); 
+});
+
+//*** HTML formatter task
+gulp.task('prettify', function() {
+
+    gulp.src('./**/*.html').
+    pipe(prettify({
+        indent_size: 4,
+        indent_inner_html: true,
+        unformatted: ['pre', 'code']
+    })).
+    pipe(gulp.dest('./'));
+});
+
+
+gulp.task('default', ['browser-sync'], function () {
+
+});
+
+gulp.task('php', function() {
+    php.server({ base: './', port: 3000, keepalive: true});
+});
+
+gulp.task('browser-sync',['php'], function() {
+    browserSync.create({
+        proxy: '127.0.0.1:3000',
+        port: 3000,
+        open: false,
+        notify: false,
+        reload: true
+    });
+});
+
+<<<<<<< HEAD
+    gulp.task('browser-sync',['php'], function() {
+        browserSync.create({
+            proxy: '127.0.0.1:3000',
+            port: 3000,
+            open: false,
+            notify: false
+        });
+    });
+=======
+
+gulp.task('watch', function(){
+
+    gulp.start('php');
+>>>>>>> a7f7a225d9674bfb606b1da4050b75fbdcaf2346
+
+    gulp.watch(['./*.php'], [reload]);
+
+    gulp.watch('./assets/scripts/**/*.js', function(){
+        gulp.start('scripts');
+        browserSync.reload();
+    });
+
+    gulp.watch('./assets/styles/**/*.css', function(){
+        gulp.start('styles');
+        browserSync.reload();
+    });
+});
